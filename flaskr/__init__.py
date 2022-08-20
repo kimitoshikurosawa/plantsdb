6934dcfa3359ed3313f70867a87806a837f97428
Kim <misagokim@gmail.com>
2022-08-20 12:20:40 +0000

    new changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -19,4 +19,31 @@ def create_app(test_config=None):
     def hello():
         return jsonify({'message': 'HELLO WORLD'})
 
+    @app.route('/plants', methods=['GET', 'POST'])
+    def get_plants():
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10
+        end = start + 10
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+        return jsonify({
+            'success': True,
+            'plants': formatted_plants[start:end],
+            'total_plants': len(formatted_plants)
+
+        })
+
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+        if plant is None:
+            abort(404)
+        else:
+            return jsonify({
+                'success': True,
+                'plant': plant.format()
+
+            })
+
     return app
